1a2c86f7e4232f5e0b4f15b1c7adcea98b0530c8
karami narani <157799613+karaminarani@users.noreply.github.com>
2024-10-02 00:26:32 +0700

    Update memeg.py

--- memeg.py
+++ memeg.py
@@ -22,13 +22,13 @@ spam_chats = []
 
 @kntl.on(events.NewMessage(pattern="^/start$"))
 async def help(event):
-  helptext = "**Halo 👋🏻!\n\nKenalin Nih, Gua Bot Tag All Yang Di Rancang Sama @Darenrorr Dengan Berbasis Python.\n\nGua Siap Membantu Lu Dengan Mention Semua Anggota Di Group Anda**"
+  helptext = "**Halo 👋🏻!\n\nKenalin Nih, Gua Bot Tag All Yang Di Rancang Sama @suheroku Dengan Berbasis Python.\n\nGua Siap Membantu Lu Dengan Mention Semua Anggota Di Group Anda**"
   await event.reply(
     helptext,
     link_preview=False,
     buttons=(
       [
-        Button.url('Developer', 't.me/darenrorr'),
+        Button.url('Developer', 't.me/suheroku'),
       ],
       [
         Button.url('Support', 't.me/Darensupport'),
