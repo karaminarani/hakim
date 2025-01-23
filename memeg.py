b91ad17edd38ff757776d01ba51491f66091a011
karami narani <157799613+karaminarani@users.noreply.github.com>
2025-01-23 12:07:26 +0700

    Update memeg.py

--- memeg.py
+++ memeg.py
@@ -32,7 +32,7 @@ async def help(event):
       ],
       [
         Button.url('Support', 't.me/hellarioshome'),
-        Button.url('Channel', 't.me/KONTEN_TIKTOK_OMETV'),
+        Button.url('Channel', 't.me/anakundua'),
       ],
     )
   )
